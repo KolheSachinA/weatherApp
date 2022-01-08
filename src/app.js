0f1b642167b23740b64088fbbb81834bd970b036
kolhe-sachin <sachinkolhe57@gmail.com>
2022-01-08 22:59:38 +0530

    asa

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 const forecast = require('../utils/forecast');
-const geocode = require('../utils/geocode')
+var geocode = require('../utils/geocode')
 const port = process.env.PORT || 3000;
 
 const app = express()
